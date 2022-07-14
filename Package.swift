1ce7402994504324b30fd24cf88ac2f59a7f3b7e
Wonyoung Ju <getogrand@gmail.com>
2022-07-14 10:57:53 +0900

    Fix package dependency urls that have been changed

--- Package.swift
+++ Package.swift
@@ -15,8 +15,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/kieranb662/Sliders.git" , from: "1.0.3"),
-        .package(url: "https://github.com/kieranb662/Shapes.git", from: "1.0.4"),
+        .package(
+            url: "https://github.com/Sling-Inc/Sliders-SwiftUI.git",
+            .revisionItem("99ba86b354f5c7ba487075d486d6ff715f76fd26")
+        ),
+        .package(url: "https://github.com/kieranb662/SwiftUI-Shapes.git", from: "1.0.4"),
         .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.3")
     ],
     targets: [
@@ -24,7 +27,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ColorKit",
-            dependencies: ["CGExtender","Shapes","Sliders"]),
+            dependencies: [
+                "CGExtender",
+                .product(name: "Shapes", package: "SwiftUI-Shapes"),
+                .product(name: "Sliders", package: "Sliders-SwiftUI")
+            ]),
         .testTarget(
             name: "ColorKitTests",
             dependencies: ["ColorKit"]),
